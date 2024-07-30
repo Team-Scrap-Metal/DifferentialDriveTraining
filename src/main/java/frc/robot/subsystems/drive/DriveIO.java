b1331e9f277ebaf6aa3d5a1d33add3483a7a2435
El1zabethS <Elizabeth.seaton35@gmail.com>
2024-07-30 15:07:30 -0700

    added base project

---
+++ src/main/java/frc/robot/subsystems/drive/DriveIO.java
@@ -0,0 +1,29 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.drive;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface DriveIO {
+  @AutoLog
+  public static class DriveIOInputs {
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(DriveIOInputs inputs) {}
+
+  /** Run open loop at the specified voltage. */
+  public default void setVoltage(double leftVolts, double rightVolts) {}
+}
